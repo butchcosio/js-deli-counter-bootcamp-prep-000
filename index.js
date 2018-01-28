13203585550d4b24559b695013b0936a13edff61
butch cosio <butchcosio@yahoo.com>
2018-01-28 05:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let l =0
 var line2= []
 while (line.length>0) {
 for (l=0; l < line.length; l++)  {
- line2.push (((l+1)+". ") +""+line[l])}
+ line2.push (((l+1)+". ") +" "+line[l])}
 return "The line is currently:" + line2
 }
 
